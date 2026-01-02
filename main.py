c16a64dd0893c5a909573cd7303a33a9122d7bee
vlad <mlprimitif@gmail.com>
2026-01-02 18:05:52 +0700

    up

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app = Flask(__name__)
 def memes():
     return """1303199992 - Memes Raja Doraja\n1302239126 - Memes""" # Change to your group's member; format (NIM - Name)
 
-@app.route("/ip-public"):
+@app.route("/ip-public")
 def getIpPublic():
     ipPublic = get_public_ip()
     return f"<h1> {ipPublic}:{PORT}</h1>"
